fadc9b1be9d2636b654fd34ed9829ecafa2765a1
monocono <matrosica0808@gmail.com>
2021-05-25 17:38:30 +0900

    del init

--- app/src/main/java/com/jjaln/dailychart/SplashActivity.java
+++ app/src/main/java/com/jjaln/dailychart/SplashActivity.java
@@ -11,17 +11,10 @@ import com.jjaln.dailychart.feature.Coin;
 import java.util.ArrayList;
 
 public class SplashActivity extends AppCompatActivity {
-    public ArrayList<Coin> CoinData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        CoinData = new ArrayList<>();
-        CoinData.add(new Coin(R.mipmap.btc, "BTC", "0", "0", "0"));
-        CoinData.add(new Coin(R.mipmap.eth, "ETH", "0", "0", "0"));
-        CoinData.add(new Coin(R.mipmap.xrp, "XRP", "0", "0", "0"));
-        CoinData.add(new Coin(R.mipmap.ada, "ADA", "0", "0", "0"));
-        CoinData.add(new Coin(R.mipmap.dot, "DOT", "0", "0", "0"));
         startLoading();
     }
     private void startLoading()
@@ -31,7 +24,6 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.putExtra("init",CoinData);
                 startActivity(intent);
                 finish();
             }
